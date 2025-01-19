50947582a1b84e0da98f7643b7207d3d8fe28f13
DamjanSarlija <148581555+DamjanSarlija@users.noreply.github.com>
2025-01-19 20:19:07 +0100

    Update index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,18 @@
 const express = require("express")
 const router = express.Router();
+const { requiresAuth } = require("express-openid-connect");
+const path = require("path");
+
+const redirectIndex = (req, res, next) => {
+    if (!req.session.status) {
+        return res.redirect('/');
+    }
+    next();
+};
 
 router.get("/", (req, res) => {
+    const isAuthenticated = req.oidc.isAuthenticated();
+    const user = req.oidc.user;
     res.sendFile("index.html")
 })
 
@@ -11,6 +22,57 @@ router.get("/preuzmiJson", (req, res) => {
 
 router.get("/preuzmiCsv", (req, res) => {
     res.download("./data/studenci.csv")
+})
+
+router.get("/status.js", (req, res) => {
+    isAuthenticated = false
+    if (req.session.status == true) {
+        isAuthenticated = true
+    }
+
+    res.type("application/javascript");
+    res.send(`
+        const isAuthenticated = ${isAuthenticated};
+    `);
+});
+
+router.get('/logiraj', (req, res) => {
+    console.log("Tu sam u login endpointu")
+    if (!req.oidc.isAuthenticated()) {
+        console.log("Nismo bili logirani. Sad se logiramo.")
+        req.session.status = true;
+        res.oidc.login();
+    } else {
+        req.session.status = true;
+        console.log("Već smo logirani. Nema potrebe ponovo.")
+        res.redirect('/');
+    }
+    
+    
+});
+
+router.get('/logoutaj', (req, res) => {
+    console.log("Tu smo u logoutu")
+    req.session.status = false
+    res.redirect('/');
+});
+
+router.get('/oidcLogout', (req, res) => {
+    req.session.status = false
+    res.oidc.logout()
+})
+
+router.get("/korisnickiProfil", redirectIndex, (req, res) => {
+    res.render("profil", {
+        name: req.oidc.user.name,
+        sub: req.oidc.user.sub,
+        email: req.oidc.user.email,
+        email_verified: req.oidc.user.email_verified
+    })
+})
+
+router.get("/osvjeziPreslike", redirectIndex, (req, res) => {
+    
 })
 
 module.exports = router
