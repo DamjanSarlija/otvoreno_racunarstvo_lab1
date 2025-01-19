a5c2661c4d0a69fc812b8aa865cace3e8215d90d
DamjanSarlija <148581555+DamjanSarlija@users.noreply.github.com>
2025-01-19 20:13:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const express = require("express");
 const session = require("express-session")
 const ir = require("./routes/index.routes");
 const dr = require("./routes/datatable.routes");
+const ar = require("./routes/api.routes");
 const path = require("path")
+const { auth } = require("express-openid-connect");
+
 
 const app = express();
 
 const port = 3000;
 
+const authConfig = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: "anything",
+    baseURL: "http://localhost:3000",
+    clientID: "Jx7y3oPNmCMRgdvONJj9BEG8BIJzni6h",
+    issuerBaseURL: "https://dev-58jxy06foitq3exp.us.auth0.com",
+};
+
+app.use(auth(authConfig));
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
 
 app.use(
     session({
@@ -26,10 +44,16 @@ app.use((err, req, res, next) => {
     res.status(500).send('Doslo je do greske');
 });
 
-
-
 app.use("/", ir);
 app.use("/datatable", dr)
+app.use("/api", ar)
 
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: "Not Found",
+        message: "Traženi endpoint ne postoji ili se koristi neispravna HTTP metoda"
+    });
+  });
 
 app.listen(port);
